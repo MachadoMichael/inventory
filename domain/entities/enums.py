acc3afa01bb0c1bedfd65ed4c30a1ec8d7f6f74a
Michael Machado <michael.machado@devtype.com.br>
2026-09-06 14:13:11 -0300

    feat: adiciona enums

---
+++ domain/entities/enums.py
@@ -0,0 +1,20 @@
+import enum
+
+class ReservationStatus(str, enum.Enum):
+    ACTIVE = "ACTIVE"
+    CONSUMED = "CONSUMED"
+    RELEASED = "RELEASED"
+
+
+class ReleaseReason(str, enum.Enum):
+    COMPENSATION = "COMPENSATION"
+    EXPIRATION = "EXPIRATION"
+    ORDER_CANCELLED = "ORDER_CANCELLED"
+
+
+class MovementType(str, enum.Enum):
+    RECEIPT = "RECEIPT"
+    RESERVE = "RESERVE"
+    RELEASE = "RELEASE"
+    DISPATCH = "DISPATCH"
+    ADJUSTMENT = "ADJUSTMENT"
